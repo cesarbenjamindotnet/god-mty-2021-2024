99a9248eab117c28fd4203f5e69557b095dc903a
Cesar Benjamin <mathereall@gmail.com>
2025-01-24 16:29:02 -0600

    b

--- javascript/jquery.validate.es.js
+++
@@ -1,8 +0,0 @@
-﻿//$.extend($.validator.messages, {
-//    required: "El campo es obligatorio",
-//    email: "El campo email no contiene un formato correcto",
-//    number: "Favor de agregar solo numeros",
-//    minlength: "Favor de agregar al menos {0} caracteres",
-//    maxlength: "Favor no agregar mas de {0} caracteres"
-
-//});
---
+++ public/javascript/moment/moment-datetime.js
@@ -0,0 +1,35 @@
+﻿/**
+ * This plug-in will provide date sorting for the "dd/mm/YYYY hh:ii:ss" 
+ * formatting, which is common in France and other European countries. It can 
+ * also be quickly adapted for other formatting as required. Furthermore, this 
+ * date sorting plug-in allows for empty values in the column.
+ *
+ * Please note that this plug-in is **deprecated*. The
+ * [datetime](//datatables.net/blog/2014-12-18) plug-in provides enhanced
+ * functionality and flexibility.
+ *
+ *  @name Date (dd/mm/YYYY hh:ii:ss) 
+ *  @summary Sort date / time in the format `dd/mm/YYYY hh:ii:ss`
+ *  @author [Ronan Guilloux](http://coolforest.net/)
+ *  @deprecated
+ *
+ *  @example
+ *    $('#example').dataTable( {
+ *       columnDefs: [
+ *         { type: 'date-moment', targets: 0 }
+ *       ]
+ *    } );
+ */
+
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+
+    "date-moment-pre": function (a) {
+        return ~~moment(a, "DD/MM/YYYY HH:mm:ss a").format("X");
+    },
+    "date-moment-asc": function (a, b) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+    "date-moment-desc": function (a, b) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+  });
